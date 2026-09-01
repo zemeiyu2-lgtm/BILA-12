0f9c602ccb0778bba668f4c49144ee04653aadf7
zemeiyu2-lgtm <zemeiyu2@gmail.com>
2026-09-01 14:11:00 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// BILA V1.9 test build: no offline cache by design.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
